9ebfc2b43ecd0d082d46a281e759c5e2a08a7190
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-02-25 09:02:28 +0100

    Synchronizing with symfony svn

--- lib/autoload/sfCoreAutoload.class.php
+++ lib/autoload/sfCoreAutoload.class.php
@@ -11,7 +11,7 @@
 /**
  * The current symfony version.
  */
-define('SYMFONY_VERSION', '1.4.3-DEV');
+define('SYMFONY_VERSION', '1.4.4-DEV');
 
 /**
  * sfCoreAutoload class.
@@ -22,7 +22,7 @@ define('SYMFONY_VERSION', '1.4.3-DEV');
  * @package    symfony
  * @subpackage autoload
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id: sfCoreAutoload.class.php 27970 2010-02-12 19:44:00Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfCoreAutoload.class.php 28270 2010-02-25 07:08:17Z Kris.Wallsmith $
  */
 class sfCoreAutoload
 {
